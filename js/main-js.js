a772b3cb53904fec32d65a8fc8a1559901e24872
comisac01 <comisac.conf@gmail.com>
2024-04-17 09:54:45 +0530

    done

--- js/main-js.js
+++ js/main-js.js
@@ -6,6 +6,8 @@ window.onload = function (e) {
             <h1 style="font-size: 2rem;font-weight:700;margin:15px 0px">2<sup>nd</sup>  International Conference on Computational<br>Research and Data Analytics  (ICCRDA-2025)</h1>
             <h2 style="font-size:1.4rem;font-weight:600;margin:15px 0px">Duy Tan University, Da Nang,Vietnam</h2>
             <p id="demo" style="font-size:1.6rem;font-weight:700"></p>
+            <h1 style="font-family: 'Merriweather', serif;font-size:1.0rem;text-transform:uppercase">ICCRDA 2025 will be jointly organsied by School of Open Learning, University of Delhi and
+            Shaheed Rajguru College of Applied Sciences, University of Delhi</h2>
             <a href="Registrations.html" class="btn btn-primary ">Register Now</a>
             </div>
             <div><img src="../duytan.png" alt="Duy Tan" width="200" height="200" hspace= "15" class="img-fluid top_image"><br>
